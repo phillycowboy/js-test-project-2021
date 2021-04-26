a9979c10e99ecf7567e65b5ace524cf32f2a7c15
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-26 15:36:13 -0400

    added edit update and destroy actions to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,18 @@ class TasksController < ApplicationController
         end
     end
 
+    def edit 
+    
+    end
+
+    def update
+    
+    end
+
+    def destroy 
+
+    end
+
     private 
 
     def task_params 
