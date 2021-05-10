f26cb831de0413e969c4eecf8e00f8028fc7b2f5
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-05-10 14:00:26 -0400

    updated User controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
     def create 
         user = User.new(user_params)
-        if user.save 
+        if user.save
             render json: user 
         else 
             render json: {message: "Could not create User."}
