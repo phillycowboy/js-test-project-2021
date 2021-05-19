f12fe28acd4f000b8898757e8e1cf74f9e252bcc
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-05-19 18:43:04 -0400

    used pry for bug

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,7 +32,7 @@ class TasksController < ApplicationController
 
     def update
         task = Task.find_by(id: params[:id])
-        binding.pry
+        # binding.pry
         if task && task.update(task_params)
             render json: task
         else 
