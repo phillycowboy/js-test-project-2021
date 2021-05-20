34c859bf82ab20151d1663c374cda56574e70624
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-05-20 12:36:00 -0400

    rails validations for presence of a name, email, and task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
     belongs_to :user 
+    validates :activity, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_many :tasks
+    validates :name, presence: true 
+    validates :email, presence: true 
 end
